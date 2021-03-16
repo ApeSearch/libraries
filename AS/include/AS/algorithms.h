8586692ab16f992b9cde6c5b2bb090644d9f9b1a
xiongrob <62448777+xiongrob@users.noreply.github.com>
2021-03-16 17:52:54 -0400

    Fix some inefficenies

--- AS/include/AS/algorithms.h
+++ AS/include/AS/algorithms.h
@@ -17,22 +17,23 @@ namespace APESEARCH
 {
 // Returns an iterator to value that is not less than value provided. (It could be either the last value or pointing to the value itself)
 template<class ForwardIterator, class T, class Predicate>
-ForwardIterator lower_bound(ForwardIterator first, ForwardIterator last, const T& val, Predicate pred) {
-    while (first < last) {
-        ForwardIterator mid = first + (last - first) / 2;
-        if (pred(*mid, val))
+ForwardIterator lower_bound(ForwardIterator first, ForwardIterator last, const T& val, Predicate pred)
+   {
+   while (first < last) {
+         ForwardIterator mid = first + (last - first) / 2;
+         if (pred(*mid, val))
             first = mid + 1;
-        else  // obj[mid] >= val
+         else  // obj[mid] >= val
             last = mid;
-    } // end while
-    return first;
-} // end lower_bound()
+   } // end while
+   return first;
+   } // end lower_bound()
 
 int stricmp(const char *a, const char *b);
 
-template<typename T> void swap(T& lhs, T& rhs) noexcept
+template<typename T> void swap( T& lhs, T& rhs ) noexcept
    {
-   T temp = std::move( lhs );
+   T temp ( std::move( lhs ) );
    lhs = std::move( rhs );
    rhs = std::move( temp );
    }
@@ -49,7 +50,7 @@ OutputIterator copy( InputIterator first, const InputIterator last, OutputIterat
 template<class InputIterator, class Function>
 Function for_each( InputIterator first, InputIterator last, Function fn )
    {
-   while (first!=last)
+   while ( first != last )
       fn ( *first++ );
    return std::move( fn );
    }
@@ -72,7 +73,7 @@ bool all_of( InputIterator first, InputIterator last, UnaryPredicate pred )
 template<class InputIterator, class T>
 InputIterator find( InputIterator first, InputIterator end, const T& val )
    {
-   for (; first != end; ++first )
+   for ( ; first != end; ++first )
       if ( *first == val )
          return first;
    return end;
