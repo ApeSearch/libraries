5894a764f51da0d123dcad078f0527cedcf0d546
Nikola Gjakovik <nikolag@umich.edu>
2021-04-03 10:36:56 -0500

    updates to socket

--- AS/src/Socket.cpp
+++ AS/src/Socket.cpp
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include <iostream>
 #include <errno.h>
+#define PORT 8080
+#define QUEUESIZE 32
 
 Socket::Socket(const Address& address, time_t seconds)
 {
@@ -27,6 +29,52 @@ Socket::Socket(const Address& address, time_t seconds)
     {
         throw;
     }
+}
+
+
+Socket::Socket(int port)
+{
+    socketFD = socket(AF_INET, SOCK_STREAM, 0);
+
+    if(socketFD < 0)
+        //TODO
+        throw;
+
+    //Set socket options to reuse address and port
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &(int){1}, sizeof(int)) < 0)
+        //TODO
+        throw;
+    
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+
+    //Use internet socket?
+    addr.sin_family = AF_INET;
+    //Let OS map it to the correct address
+    addr.sin_addr.s_addr = INADDR_ANY;
+    
+    addr.sin_port = htons(PORT);
+
+    //Bind socket
+    if(bind(socketFD, (sockaddr *) &addr, sizeof(addr)) < 0)
+        //TODO
+        throw;
+
+    //Mark this socket as listening passively    
+    if(listen(socketFD, QUEUESIZE) < 0)
+        //TODO
+        throw;
+}
+
+Socket::Socket() : socketFD(-1){}
+
+Socket Socket::accept(struct sockaddr *addr, socklen_t *addrlen);
+{
+        int connectionFD = accept(socketFD, addr, addrlen);
+        Socket socket();
+        socket.socketFD = connectionFD;
+
+        return socket;
 }
 
 Socket::~Socket() 
