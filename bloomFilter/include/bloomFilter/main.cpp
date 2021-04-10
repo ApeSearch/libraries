6c66f6b70496dbd64d72802ff4712f1e870c7c64
Shi Ya Wong <alexwong@umich.edu>
2021-04-09 23:07:30 -0400

    bloom filter mmaped

---
+++ bloomFilter/include/bloomFilter/main.cpp
@@ -0,0 +1,56 @@
+#include "BloomFilter.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    if (argc != 1) {
+        cout << "Usage: " << argv[0] << endl;
+        return 1; 
+    }
+
+    string size, error, filename, input;
+    cout << "Running bloom filter...\n";
+    cout << "Enter 'd' for default init, or 'c' for custom: ";
+    cin >> input;
+    if (input != "d") {
+        cout << "Enter estimated number of objects: ";
+        cin >> size;
+        cout << "Enter false positive error rate: ";
+        cin >> error;
+        cout << "Enter filename to initialize bloom filter: ";
+        cin >> filename;
+    }
+    else {
+        size = "1000000";
+        error = ".0001";
+        filename = "seen.txt";
+    }
+    cout << "Initializing bloom filter...\n\n";
+
+    Bloomfilter bf(stoi(size), stod(error), "bloomfilter.bin");
+    ifstream fin(filename);
+    if (!fin.is_open()) {
+        cout << "Error opening: " << filename << endl;
+        return 1;
+    }
+
+    uint64_t counter = 0;
+    while(fin >> input) {
+        //bf.insert(APESEARCH::string( input.begin(), input.end() ));
+        counter++;
+    }
+
+    cout << "Bloom filter initialized:\n\tItems inserted: " << counter << "\n\tEstimated Total Size: " << size << "\n\tFalse positive error rate: " << error << "\n\tFilename: " << filename <<
+        "\nEnter a string to check if the bloom filter contains or enter 'q' to quit.\n";
+
+    while (true) {
+        cin >> input;
+        if (input == "q") return 0;
+        bf.contains(APESEARCH::string( input.begin(), input.end() ) ) ? cout << "Found!\n" : cout << "Not found.\n";
+    }
+
+    return 0;
+}
